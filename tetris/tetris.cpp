1acdc99b1a957ab35a1ef258f4f5d8b61606edd0
di <git@github.com>
2022-05-17 00:39:55 +0900

    ■の垂直落下

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -2,10 +2,76 @@
 //
 
 #include <iostream>
+#include <Windows.h>
+
+#define COLOR_BLACK      0x00
+#define COLOR_BLUE       0x09
+#define COLOR_GREEN      0x0a
+#define COLOR_CYAN       0x0b
+#define COLOR_RED        0x0c
+#define COLOR_VIOLET     0x0d
+#define COLOR_YELLOW     0x0e
+#define COLOR_WHITE      0x0f
+#define COLOR_INTENSITY  0x08
+#define COLOR_RED_MASK   0x04
+#define COLOR_GREEN_MASK 0x02
+#define COLOR_BLUE_MASK  0x01
+
+using namespace std;
+
+void setCursorPosition(int x, int y)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD p;
+    p.X = x;
+    p.Y = y;
+    SetConsoleCursorPosition(h, p);
+}
+
+void setColor(int color)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    WORD a = 0;
+    if (color & COLOR_INTENSITY)
+        a |= BACKGROUND_INTENSITY;
+    if (color & COLOR_RED_MASK)
+        a |= BACKGROUND_RED;
+    if (color & COLOR_GREEN_MASK)
+        a |= BACKGROUND_GREEN;
+    if (color & COLOR_BLUE_MASK)
+        a |= BACKGROUND_BLUE;
+    SetConsoleTextAttribute(h, a);
+}
+
+void moveMino()
+{
+    int x = 50;
+    int y = 0;
+
+    while (y < 20)
+    {
+        setColor(COLOR_CYAN);
+        setCursorPosition(x, y);
+        cout << "  ";
+        Sleep(1000);
+
+        int _x = x;
+        int _y = y;
+        x = x;
+        y++;
+        setColor(COLOR_BLACK);
+        setCursorPosition(_x, _y);
+        cout << "  ";
+    }
+
+    setColor(COLOR_WHITE);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    moveMino();
+
+    return 0;
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
